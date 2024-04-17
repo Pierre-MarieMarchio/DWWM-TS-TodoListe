7c29cfb072afe9d902464cf61583e36d7b2dd48c
Pierre-Marie <pierremariemarchio@gmail.com>
2024-04-17 16:57:36 +0200

    refactoring and fix tentativ fot eddit task

--- src/libs/btnTaskLogique.ts
+++ src/libs/btnTaskLogique.ts
@@ -5,36 +5,82 @@ const getTaskId = (target: HTMLElement): string | null => {
   const parentElement = target.parentNode;
   const grandParentElement = parentElement?.parentNode as HTMLElement | null;
 
-  if (grandParentElement) {
-    const parentElementID = grandParentElement.getAttribute("id");
+  // console.log(grandParentElement);
+  // console.log(parentElement);
 
-    return parentElementID;
+  if (grandParentElement) {
+    const taskID = grandParentElement.getAttribute("id");
+
+    return taskID;
   }
 
   return null;
 };
 
-const handleDeleteBtn = (target: HTMLElement, list: List) => {
-   const taskID = getTaskId(target);
-      if (taskID) {
-        renderRemoveTask(taskID);
-        list.serviceRemoveTask(taskID);
+const handleDeleteBtn = (target: HTMLElement, list: List): void => {
+  const taskID = getTaskId(target);
+  if (taskID) {
+    renderRemoveTask(taskID);
+    list.serviceRemoveTask(taskID);
+  }
+};
+
+const handleEditMethods = (target: HTMLElement): void => {
+  const taskID = getTaskId(target);
+  const taskBox = target.parentNode;
+  const taskText: HTMLElement | null | undefined = taskBox?.querySelector("label.postit-text");
+
+  console.log(target);
+  console.log(taskBox);
+
+  if (taskID && taskBox instanceof HTMLElement) {
+    const handleBlur = () => {
+      console.log("coucou");
+
+      taskBox?.classList.remove("ismodified");
+      taskText?.removeEventListener("blur", handleBlur);
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        taskBox?.classList.remove("ismodified");
+        taskText?.blur();
+        taskText?.removeEventListener("keydown", handleKeyDown);
       }
+    };
+
+    if (taskBox.classList.contains("ismodified")) {
+
+      taskText?.addEventListener("blur", handleBlur);
+      taskBox.classList.remove("ismodified");
+      taskText?.setAttribute("contenteditable", "false");
+      taskText?.blur();
+       
+    } else {
+      taskBox.classList.add("ismodified");
+      taskText?.setAttribute("contenteditable", "true");
+      taskText?.focus();
+      
+    }
+
+   
+    taskText?.addEventListener("keydown", handleKeyDown);
+  }
 };
 
 const btnTaskLogique = (list: List) => {
-  const lists: HTMLElement | null = document.querySelector("div.list");
+  // const lists: HTMLElement | null = document.querySelector("div.list");
 
-  lists?.addEventListener("click", (e: MouseEvent) => {
+  document.addEventListener("click", (e: MouseEvent) => {
     if (e.target instanceof HTMLElement) {
       const target = e.target;
       if (target.classList.contains("delete-button")) {
         handleDeleteBtn(target, list);
-      } 
-      
-     
+      } else if (target.classList.contains("list-text")) {
+        handleEditMethods(target);
+      }
     }
   });
 };
 
-export {btnTaskLogique}
+export { btnTaskLogique };
--- src/services/list.ts
+++ src/services/list.ts
@@ -7,6 +7,7 @@ export class List {
   constructor(tasksList: TasksList) {
     this.tasksList = tasksList;
   }
+
   //@TODO - WHEN LOCAL STORAGE DELETE AND USE SERVICE
 
   serviceAddTask(formResult: TaskData): void {
@@ -32,140 +33,9 @@ export class List {
 
   editTask(): void {}
 
+
   // ENDS HERE
-
-  // addEventListener(): void {
-  //   const lists: HTMLElement | null = document.querySelector("div.list");
-
-  //   lists?.addEventListener("click", (e: MouseEvent) => {
-  //     const target = e.target as HTMLElement;
-
-  //     if (e.target instanceof HTMLElement && target.classList.contains("delete-button")) {
-  //       const parentElement = target.parentNode;
-  //       const grandParentElement = parentElement?.parentNode as HTMLElement | null;
-
-  //       if (grandParentElement) {
-  //         const parentElementID = grandParentElement.getAttribute("id");
-
-  //         if (parentElementID != null) {
-  //           this.removeTask(parentElementID);
-  //         }
-  //       }
-  //     }
-
-  //     if (e.target instanceof HTMLElement && e.target.classList.contains("list-text")) {
-  //       const parentElement = target.parentNode;
-  //       const grandParentElement = parentElement?.parentNode as HTMLElement | null;
-
-  //       if (grandParentElement) {
-  //         const parentElementID = grandParentElement.getAttribute("id");
-
-  //         if (parentElementID != null) {
-  //           const childPostitBox = grandParentElement.querySelector("div.postit-box");
-  //           const childPostitText = grandParentElement.querySelector("label.postit-text");
-
-  //           if (childPostitBox && childPostitText instanceof HTMLElement) {
-  //             if (childPostitBox.classList.contains("ismodified")) {
-  //               childPostitBox.classList.remove("ismodified");
-  //               childPostitText?.blur();
-  //             } else {
-  //               childPostitBox.classList.add("ismodified");
-  //               childPostitText.setAttribute("contenteditable", "true");
-  //               childPostitText.focus();
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   });
-  // }
-
-  handlePostitModification(postitText: HTMLElement, postitBox: HTMLElement, target: HTMLElement): void {
-    postitText.setAttribute("contenteditable", "true");
-    postitText.focus();
-
-    console.log(target);
-    console.log(postitBox);
-
-    const handleBlur = () => {
-      console.log("coucou");
-
-      postitBox?.classList.remove("ismodified");
-      postitText.blur();
-      postitText.removeEventListener("blur", handleBlur);
-    };
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Enter") {
-        postitBox?.classList.remove("ismodified");
-        postitText.blur();
-        postitText.removeEventListener("keydown", handleKeyDown);
-      }
-    };
-
-    postitText.addEventListener("blur", handleBlur);
-    postitText.addEventListener("keydown", handleKeyDown);
-  }
-
-  getTaskId(target: any) {
-    if (target instanceof HTMLElement && target.classList.contains("delete-button")) {
-      const parentElement = target.parentNode;
-      const grandParentElement = parentElement?.parentNode as HTMLElement | null;
-
-      if (grandParentElement) {
-        const parentElementID = grandParentElement.getAttribute("id");
-
-        if (parentElementID != null) {
-          this.serviceRemoveTask(parentElementID);
-          this.renderRemoveTask(parentElementID);
-        }
-      }
-    }
-  }
-
-  addEventListener(): void {
-    const lists: HTMLElement | null = document.querySelector("div.list");
-
-    lists?.addEventListener("click", (e: MouseEvent) => {
-      if (e.target instanceof HTMLElement) {
-        const target = e.target as HTMLElement;
-        this.getTaskId(target);
-
-        if (e.target instanceof HTMLElement && e.target.classList.contains("list-text")) {
-          const parentElement = target.parentNode;
-          const grandParentElement = parentElement?.parentNode as HTMLElement | null;
-
-          if (grandParentElement) {
-            const parentElementID = grandParentElement.getAttribute("id");
-
-            if (parentElementID != null) {
-              const childPostitBox = grandParentElement.querySelector("div.postit-box");
-              const childPostitText = grandParentElement.querySelector("label.postit-text");
-
-              if (childPostitBox) {
-                if (childPostitBox.classList.contains("ismodified")) {
-                  childPostitBox.classList.remove("ismodified");
-                } else {
-                  childPostitBox.classList.add("ismodified");
-                }
-              }
-              if (childPostitText instanceof HTMLElement && childPostitBox instanceof HTMLElement) {
-                this.handlePostitModification(childPostitText, childPostitBox, e.target);
-              }
-            }
-          }
-        }
-      }
-    });
-  }
-
-  renderRemoveTask(taskId: String): void {
-    const task = document.getElementById(`${taskId}`) as HTMLElement | null;
-    if (task) {
-      task.remove();
-    }
-  }
-
+ 
   updateList(): void {}
 
   returnTasksList(): TasksList {
